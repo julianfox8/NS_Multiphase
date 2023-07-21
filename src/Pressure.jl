065df2b2e7bc5b663b04e6d3c20ebf03c77a991f
julianfox8 <julianfox8@gmail.com>
2023-07-20 18:03:15 -0600

    constructor_time

--- src/Pressure.jl
+++ src/Pressure.jl
@@ -202,6 +202,17 @@ function Secant_jacobian!(P,uf,vf,wf,gradx,grady,gradz,band,dt,denx,deny,denz,pa
     @unpack tol,Nx,Ny,Nz = param
     @unpack imin_,imax_,jmin_,jmax_,kmin_,kmax_,imino_,imaxo_,jmino_,jmaxo_,kmino_,kmaxo_ = mesh
     @unpack dx,dy,dz = mesh
+    println("test poisson structure")
+    P = Poisson(P,uf,vf,wf,param,mesh,par_env)
+    m,n,o = size(P.f[1])
+    for ii in 1:length(P.f)
+        @loop param for k=1:o-1, j=1:n-1, i=1:m-1 
+            P.f[ii][i,j,k] += del(i,I,p.p,p.f,den,dt)
+        end
+    end
+
+    error("stop")
+
     AP = OffsetArray{Float64}(undef, imin_:imax_,jmin_:jmax_,kmin_:kmax_)
     fill!(AP,0.0)
     # outflowCorrection!(RHS,AP,uf,vf,wf,P,dt,gradx,grady,gradz,band,param,mesh,par_env)
@@ -218,7 +229,7 @@ function Secant_jacobian!(P,uf,vf,wf,gradx,grady,gradz,band,dt,denx,deny,denz,pa
         # compute jacobian
         J = computeJacobian(P,uf,vf,wf,gradx,grady,gradz,band,dt,param,denx,deny,denz,mesh,par_env)
 
-        P[imin_:imax_,jmin_:jmax_,kmin_:kmax_] .-= 0.5AP./̂J
+        P[imin_:imax_,jmin_:jmax_,kmin_:kmax_] .-= 0.8AP./̂J
 
         P .-=mean(P)
 
@@ -364,3 +375,97 @@ function conjgrad!(P,RHS,param,mesh,par_env)
     
     return length(RHS)
 end
+
+#want to define step and del operators
+CI(a...) = CartesianIndex(a...)
+step(i,::Val{N}) where N = CI(ntuple(j -> j==i ? 1 : 0, N))
+step(i,I::CartesianIndex{N}) where N = step(i, Val{N}())
+
+# partial derivative of scalar field
+@inline del(a,I::CartesianIndex{d},f::AbstractArray{T,d}) where {T,d} = @inbounds f[I]-f[I-step(a,I)]
+# partial derivative of vector field
+@inline del(a,I::CartesianIndex{m},u::AbstractArray{T,n}) where {T,n,m} = @inbounds u[I+step(a,I),a]-u[I,a]
+#might want to define a del function in the a_th direction that takes the correpsonding flow field, density and dt
+@inline del(a,I::CartesianIndex{m},u::AbstractArray{T,n}, f::AbstractArray{T,d},den::AbstractArray{T,n},dt::Float64) where {T,n,m,d} = @inbounds u[I]-dt/den*del(a,I,f)
+
+#! need an A! that recieves cartesian indices for local A calc
+function A!(P::Poisson{T}) where {T}
+    param,mesh = P.param,P.mesh
+    @unpack dx,dy,dz = mesh
+
+    # for i in 1:m
+    #     p.f[I,i] -= del(i,I,p.p,p.f,den,dt) over I in 1:length(p.f[i]) #!need to create a loop macro similar to waterlily.jl
+    # end
+
+    for i in 1:length(P.f)
+        P.f[i][I] +=1
+    end
+    #! convert crtesian index to i,j,k indices
+    if abs(band[i,j,k]) <= 1
+        tets, inds = cell2tets_withProject_uvwf(i,j,k,p.f[1],p.f[2],p.f[3],dt,mesh)
+        if any(isnan,tets)
+            error("Nan in tets at ", i,j,k)
+        end
+        v2 = dx*dy*dz
+        v1 = tets_vol(tets)
+        p.z[i,j,k] = (v2-v1) /̂ v2 /̂ dt #? need to think about whether we use I or i,j,k
+    else 
+            # Calculate divergence with finite differnce
+        du_dx = del(i,I,p.f[1])/̂(dx)
+        dv_dy = del(j,I,p.f[2])/̂(dy)
+        dw_dz = del(k,I,p.f[3])/̂(dz)
+        p.z[I] = du_dx + dv_dy + dw_dz
+    end
+end
+
+
+# function Jacobian!(p::Poisson)
+#     delta = 1.0
+#     #! need to loop over p-field
+#         #! calc jacobian at each pt in mesh
+#         J = ...
+#     p.p .-= 0.8p.z/J
+
+
+# end
+#recieve gradients and face velocities and calculate the divergence dependent upon the value of the band
+#also want a jacobian operator
+struct Poisson{T<:AbstractArray,parameters,par_env_struct,mesh_struct}
+    p :: T # Pressure
+    f :: Tuple{T,T,T} # flow (can we store three objects of type T here or do we need individual face velocity arrays)
+    z :: T # source
+    e :: T # error #? do these also need to be of type T or just a float value?
+    r :: T # residual
+    n :: Vector{Int16} # iterations
+    param :: parameters # param object
+    par :: par_env_struct # parallel environement structure
+    mesh :: mesh_struct # mesh structure
+    function Poisson(p::T,uf::T,vf::T,wf::T,param::parameters,par::par_env_struct,mesh::mesh_struct) where {T,parameters,par_env_struct,mesh_struct} #? is this where argument redundant
+        #need to initilize all types 
+        f = (uf,vf,wf)
+        #want to compute grad(u*-dt/rho) in each direction and store it in f 
+        r = similar(p); fill!(r,0.0)
+        z,e = copy(r),copy(r)
+        new{T,typeof(param),typeof(par),typeof(mesh)}(p,f,z,e,r,[],param,par,mesh)
+        #new(p,f,z,e,r,[],param,par,mesh)
+    end
+end
+
+# function Jacobi!(P::Poisson,tol=1e-8)
+#     p,f,z,r,n = P.p,P.f,P.z,P.r,P.n
+#     #! calc A(P) as p.z
+#     p.z = A!(P)
+
+#     n = 0
+#     while true 
+#         n +=1
+        
+#         #! calc jacobian
+#         Jacobian!(p)
+#         #! update p
+    
+#         #! calc A(P)
+        
+#         #! check residual to tol for new A(P)
+# end
+
